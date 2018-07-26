4370ddea44388cf2f0dab3715e31b1bbb6e77a2d
yuppieghost <344387990@qq.com>
2018-07-26 21:01:00 +0800

    tarena

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,12 +5,14 @@ from django.db.models import Q
 from django.views.generic.base import View
 
 from .models import UserProfile
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
+
 
 # Create your views here.
 
 class CustomBackend(ModelBackend):
     '''自定义登录'''
+
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
             user = UserProfile.objects.get(Q(username=username) | Q(email=username))
@@ -27,14 +29,20 @@ class LoginView(View):
     def post(self, request):
         login_form = LoginForm(request.POST)
         if login_form.is_valid():
-            pass
-        user_name = request.POST.get('username', '')
-        pass_word = request.POST.get('password', '')
-        user = authenticate(username=user_name, password=pass_word)
-        if user:
-            login(request, user)
-            return render(request, 'index.html')
+
+            user_name = request.POST.get('username', '')
+            pass_word = request.POST.get('password', '')
+            user = authenticate(username=user_name, password=pass_word)
+            if user:
+                login(request, user)
+                return render(request, 'index.html')
+            else:
+                return render(request, 'login.html', {'msg': '用户名或密码错误!'})
         else:
-            return render(request, 'login.html', {'msg': '用户名或密码错误'})
+            return render(request, 'login.html', {'login_form': login_form})
 
 
+class RegisterView(View):
+    def get(self, request):
+        register_form = RegisterForm()
+        return render(request, 'register.html', {'register_form': register_form})
