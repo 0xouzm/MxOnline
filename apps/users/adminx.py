8ed881c35e5a007ce0acda067c515f17f85ec35b
yuppieghost <344387990@qq.com>
2018-07-26 14:13:07 +0800

    Initial commit

---
+++ apps/users/adminx.py
@@ -0,0 +1,32 @@
+import xadmin
+
+from .models import *
+from xadmin import views
+
+
+class BaseSetting:
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings:
+    site_title = 'Rick Online'
+    site_footer = 'Rick and Morty'
+    menu_style = 'accordion'
+
+class EmailVerifyRecordAdmin(object):
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+class BannerAdmin:
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
